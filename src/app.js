cb96a231dd415e80c9c187ab9ee44613bc7179df
kevd82 <80932709+kevd82@users.noreply.github.com>
2022-09-17 18:54:51 -0700

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -7,7 +7,16 @@ export class App {
     this.todoList.push(new Todo("Exercise"));
     this.todoList.push(new Todo("Read Conan"));
     this.todoList.push(new Todo("Algorithms"));
+    this.newItem="";
 
   }
 
+  addTodo() {
+    this.todoList.push(new Todo(this.newItem));
+    this.newItem=""; 
+  }
+
+  removeTodo(todo){
+    this.todoList.splice(this.todoList.indexOf(todo), 1);
+  }
 }
